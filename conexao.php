08bdd7795175a707203c711e01dbd818831b9ce4
analistaPedrotech <155647125+analistaPedrotech@users.noreply.github.com>
2024-05-09 18:44:15 -0300

    Tela de Login

---
+++ conexao.php
@@ -0,0 +1,12 @@
+<?php
+
+$usuario = 'root';
+$senha = 'admin1554';
+$database = 'login';
+$host = 'localhost';
+
+$mysqli = new mysqli($host, $usuario, $senha, $database);
+
+if($mysqli->error) {
+    die("Falha ao conectar ao banco de dados: " . $mysqli->error);
+}
